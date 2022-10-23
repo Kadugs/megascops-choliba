71ad672d29f4e4ee8a4dd4fbd0242f2cc088c5d9
Gabriel Teixeira <Gteixeiraca@gmail.com>
2022-10-22 23:33:01 -0300

    feat: run server on port 3000

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import {app} from './config/express-config';
+import {Request, Response} from 'express';
+
+app.listen(3000, () => {
+	console.log('Server is running on port 3000');
+});
+
+app.get('/bolinha', (req: Request, res: Response) => {
+	res.json({
+		message: 'Hello World'
+	});
+});
